1867c80071374da852af94ecf13380e8e3330fc9
AugustoPerboni <augustoperboni@tecnico.ulisboa.pt>
2022-12-19 12:51:45 +0000

    Code reordering
    The code was reorder in order to use free load spaces and prevent unnecessary stalls.

--- 5-stage-processor-with-hazard-detection-and-forwarding/assembly-code.s
+++ 5-stage-processor-with-hazard-detection-and-forwarding/assembly-code.s
@@ -22,6 +22,7 @@ n:	.word  0
 # NOTE: Upon start, the Global-Pointer (gp=x3) points to the beginning of .data section
         addi x11, x3, 0		# x11 - a's left index
         addi x13, x11, 48	# x13 - b's left index
+<<<<<<< Updated upstream
 	addi x12, x13, -4	# x12 - a's right index
 
 	lw x14, 100(x3)	    	# x14 - n - index distance accumulator
@@ -63,6 +64,45 @@ while:  add x20, x13, x16	# x20 = &b[i]
 end:	srai x14 ,x14, 2 	# Conversion from bytes to words
         sw x14, 100(x3)		# store n's final value
 	sw x15, 96(x3)		# store x's final value	
+        addi x12, x13, -4	# x12 - a's right index
+
+	lw x14, 100(x3)	    	# x14 - n - index distance accumulator
+	lw x15, 96(x3)	   	# x15 - x = 1
+	li x16, 0	        # x16 - i 
+
+# while (b[1] > 0)
+while:  add x20, x13, x16	# x20 = &b[i]
+        lw x21, 0(x20)	  	# x21 = b[i]
+        lw x22, 0(x11)		# x22 = a[i]
+        lw x23, 0(x12)		# x23 = a[N-1-i]
+        blez x21, end		# if b[i] <= 0 end the loop
+             
+	add x22, x22, x23   	# x22 = a[i] + a[N-1-i]
+	mul x15, x15, x22	# x15 = x15*x22, >>>(x *= a[i] + a[N-1-i])<<<
+
+	sub x22, x12, x11	# x22 = 4*((N-1-i)-i), size of bytes between (N-1-i) - i
+	add x14, x14, x22	# (n += (N-1-i) – i), but keeping the values in bytes no words
+################################################
+        lw x21, 4(x20)	    	# x21 = b[i+1]
+        lw x22, 4(x11)		# x22 = a[i+1]             
+	lw x23, -4(x12)	    	# x23 = a[N-1-(i+1)]
+        blez x21, end		# if b[i+1] <= 0 end the loop
+	            
+	add x22, x22, x23   	# x22 = a[i] + a[N-1-i]
+	mul x15, x15, x22	# >>>(x *= a[i+1] + a[N-1-(i+1)])<<<
+
+        sub x22, x12, x11	# x22 = 4*((N-1-i)-i), size of bytes between (N-1-(i+1) - (i+1))
+        addi x22, x22, -8   	# Correction of i value
+	add x14, x14, x22	# (n += (N-1-i) – i), but keeping the values in bytes no words
+
+	addi x16, x16, 8  	# 2*i++
+	addi x11, x11, 8
+	addi x12, x12, -8
+	jal  x0, while
+
+end:	srai x14 ,x14, 2 	# Conversion from bytes to words
+        sw x14, 100(x3)  	# store n's final value
+	sw x15, 96(x3)	 	# store x's final value	
         addi a7, x0, 10
 	ecall
   
